4fd12ff74b8f055c2a853a7a22a260f85af6fdf2
李林 <ww1095@163.com>
2016-09-08 08:14:25 +0800

    1

---
+++ BackgroundVideoTest/app/src/main/java/com/huan/percy/backgroundvideotest/MainActivity.java
@@ -0,0 +1,54 @@
+package com.huan.percy.backgroundvideotest;
+
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.VideoView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private MediaPlayer mp = null;
+    private VideoView myVideoView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initView();
+
+        final String videoPath = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.background_gif).toString();
+
+        myVideoView.setVideoPath(videoPath);
+        myVideoView.start();
+        myVideoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+
+            @Override
+            public void onPrepared(MediaPlayer mp) {
+
+                mp.start();
+                mp.setLooping(true);
+
+            }
+        });
+
+        myVideoView.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                    @Override
+                    public void onCompletion(MediaPlayer mp) {
+
+                        myVideoView.setVideoPath(videoPath);
+                        myVideoView.start();
+
+                    }
+                });
+    }
+
+    private void initView() {
+
+        myVideoView = (VideoView) findViewById(R.id.videoView);
+
+    }
+
+}
